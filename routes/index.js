567a8527677b4533156f34eda4dff1dc399fc02d
acottam <adam@gofishcreative.com>
2026-03-20 23:29:01 -0700

    Update README.md and Default Route and Description

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ const router = require('express').Router();
 router.use('/', require('./swagger'));
 
 router.get('/', (req, res) => {
-    //#swagger.tags = ['Hello World']
-    res.send('Hello World');
+    //#swagger.tags = ['Welcome']
+    res.send('Welcome to the National Parks and Activities API');
 });
 
 router.use('/parks', require('./parks'));
--- swagger.js
+++ swagger.js
@@ -3,7 +3,7 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'CSE341 Project 2 API',
-    description: 'API for CSE341 Project 2',
+    description: 'API for exploring National Parks and Activities',
   },
   host: 'cse341-project-1-bo77.onrender.com',
   schemes: ['http', 'https'],
